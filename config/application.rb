65b38562c6215ecd3615ebd3d2d6b615c067c78d
Artur Smirnov <mail@artursmirnov.com>
2017-07-27 18:36:02 +0000

    Replace test_unit by rspec

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module SimpleCrawler
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
